169f679082fcaecd6a3988e412ad96aabd07d8ef
Stanislav Gafurov <gafurovstanislav@gmail.com>
2022-08-16 14:07:07 +0300

    fixed cyrillic locator

--- Task 7/test7.js
+++ Task 7/test7.js
@@ -9,7 +9,7 @@ describe('Test that checks if aimprosoft.com main page can be opened', function 
     it('TEST', async function () {
 
         await driver.get('http://www.google.com/ncr');
-        await driver.findElement(By.css("[title='Пошук']")).sendKeys('aimprosoft', Key.RETURN);
+        await driver.findElement(By.name("q")).sendKeys('aimprosoft', Key.RETURN);
         await driver.wait(until.titleContains('aimprosoft'), 10000);
         await driver.findElement(By.css('a[href="https://www.aimprosoft.com/"]')).click();
         let titleText = await driver.findElement(By.xpath('//*[(contains(text(), "Enterprise software"))]')).getText();
