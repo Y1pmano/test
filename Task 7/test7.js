08ec4c888267954dbca3c0fed662cfce1f899f8a
Stanislav Gafurov <gafurovstanislav@gmail.com>
2022-08-16 10:29:38 +0300

    fixed selector

--- Task 7/test7.js
+++ Task 7/test7.js
@@ -10,7 +10,7 @@ describe('Test that checks if aimprosoft.com main page can be opened', function 
 
         await driver.get('http://www.google.com/ncr');
         await driver.findElement(By.css("[title='Пошук']")).sendKeys('aimprosoft', Key.RETURN);
-        await driver.wait(until.titleContains('aimprosoft'), 100000);
+        await driver.wait(until.titleContains('aimprosoft'), 10000);
         await driver.findElement(By.css('a[href="https://www.aimprosoft.com/"]')).click();
         let titleText = await driver.findElement(By.xpath('//*[(contains(text(), "Enterprise software"))]')).getText();
         assert.equal('Enterprise software development company', titleText)
